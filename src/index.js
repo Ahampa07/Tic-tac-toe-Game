d09c08a051aa3bae25f2e87c8814a66e82025868
Ahampa07 <hakamramahampa@gmail.com>
2020-10-27 17:02:11 +0530

    first

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './components/Game';
+import './index.css';
+
+
+
+ReactDOM.render(
+  <div>
+    <Game />
+  </div>,
+  document.getElementById('root')
+);
+
+
+
+
